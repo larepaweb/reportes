5095523b34b0a67a822f8cbe2b9bc8acd04b2d70
sosaheri <sosaheriberto2001@gmail.com>
2021-09-07 14:12:42 -0400

    feat: integrate helper to get logo url

--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -4,7 +4,7 @@
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute right-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
         <a class="navbar-brand m-0" href="{{ route('dashboard') }}">
-            <img src="../assets/img/logos/logo-dex.png" style="width: auto; max-height: 55px;" class="h-50" alt="...">
+            <img src="../assets/img/logos/{{ get_setting_value( 'logo' ) }}" style="width: auto; max-height: 55px;" class="h-50" alt="...">
         </a>
     </div>
     <hr class="horizontal dark mt-0">
